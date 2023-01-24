500e120bdc0d65fb77df69c4fdbe5be66ba26a01
t-stuart <tstuart804@gmail.com>
2023-01-24 14:59:40 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ea879ee6-72f8-4dde-912a-9d97b2a3f621';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,22 @@
+import View from './View.js';
+import PreviewView from './previewView.js';
+import icons from 'url:../../img/icons.svg';
+import previewView from './previewView.js';
+
+class BookMarksView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it :)';
+  _message = '';
+
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
+
+  _generateMarkup() {
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
+  }
+}
+
+export default new BookMarksView();
